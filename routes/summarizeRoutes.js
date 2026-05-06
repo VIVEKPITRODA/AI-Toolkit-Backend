67790c0a1b5f74f2576e087537c5976d95f663d2
Vivek Pitroda <vivekpitroda75@gmail.com>
2026-05-06 18:57:39 -0400

    Final latest backend changes

--- routes/summarizeRoutes.js
+++ routes/summarizeRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { summarizeText } = require('../controllers/summarizeController');
+const { summarizeText, getHistory } = require('../controllers/summarizeController');
 const { protect } = require('../middleware/auth');
 
 router.post('/', protect, summarizeText);
-
+router.get('/history', protect, getHistory);  // ← added
 
 module.exports = router;
